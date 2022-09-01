990736c57baa6342b69b4e252a0d69092b5a4f67
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-09-01 11:24:27 -0400

    png X2 and vid times entered

--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -23,102 +23,102 @@ const state = reactive({
   playing: false,
   tuts: [
     {
-      subject: 'Chapter 1 - Ways to Win',
+      subject: 'Ways to Win',
       videos: [
         {
           id: 1,
           title: 'Prizes',
           video: '/video/01-01.mp4',
-          length: 13,
+          length: 15.04,
           marker: 0,
         },
         {
           id: 2,
           title: 'Exact',
           video: '/video/01-02.mp4',
-          length: 9,
+          length: 10.539,
           marker: 0,
         },
         {
           id: 3,
           title: 'Any Order',
           video: '/video/01-03.mp4',
-          length: 9,
+          length: 10.54,
           marker: 0,
         },
         {
           id: 4,
           title: '50/50',
           video: '/video/01-04.mp4',
-          length: 11,
+          length: 11.54,
           marker: 0,
         },
         {
           id: 5,
           title: 'Fireball',
           video: '/video/01-05.mp4',
-          length: 12,
+          length: 13.035,
           marker: 0,
         },
         {
           id: 6,
           title: 'More Ways to Win',
           video: '/video/01-06.mp4',
-          length: 9,
+          length: 10.03,
           marker: 0,
         },
       ],
     },
     {
-      subject: 'Chapter 2 - How to Play',
+      subject: 'How to Play',
       videos: [
         {
           id: 7,
           title: 'Playslip',
           video: '/video/02-01.mp4',
-          length: 13,
+          length: 9.49,
           marker: 0,
         },
         {
           id: 8,
           title: 'Play Type',
           video: '/video/02-02.mp4',
-          length: 9,
+          length: 7.04,
           marker: 0,
         },
         {
           id: 9,
           title: 'Selecting Numbers',
           video: '/video/02-03.mp4',
-          length: 9,
+          length: 6.12,
           marker: 0,
         },
         {
           id: 10,
           title: 'Quick Pick',
           video: '/video/02-04.mp4',
-          length: 11,
+          length: 10.03,
           marker: 0,
         },
         {
           id: 11,
           title: 'Selecting Drawings',
           video: '/video/02-05.mp4',
-          length: 12,
+          length: 10.03,
           marker: 0,
         },
         {
           id: 12,
           title: 'Wager',
           video: '/video/02-06.mp4',
-          length: 9,
+          length: 7.04,
           marker: 0,
         },        
         {
           id: 13,
           title: 'Fireball',
           video: '/video/02-07.mp4',
-          length: 9,
+          length: 14.04,
           marker: 0,
         },
       ],
@@ -130,47 +130,47 @@ const state = reactive({
           id: 14,
           title: 'Ticket Front',
           video: '/video/03-01.mp4',
-          length: 12.959,
+          length: 13.03,
           marker: 0,
         },
         {
           id: 15,
           title: 'Ticket Back',
           video: '/video/03-02.mp4',
-          length: 212.56,
+          length: 8.02,
           marker: 0,
         },
       ],
     },
     {
-      subject: 'Chapter 4 - Check Results',
+      subject: 'Check Results',
       videos: [
       {
           id: 16,
           title: 'Find Results',
           video: '/video/04-01.mp4',
-          length: 12.959,
+          length: 17.536,
           marker: 0,
         },
         {
           id: 17,
           title: 'Fireball (two clips)',
           video: '/video/04-02.mp4',
-          length: 212.56,
+          length: 13.03,
           marker: 0,
         },
         {
           id: 18,
           title: 'Check you ticket',
           video: '/video/04-03.mp4',
-          length: 12.959,
+          length: 10.03,
           marker: 0,
         },
         {
           id: 19,
           title: 'Find Past Results',
           video: '/video/04-04.mp4',
-          length: 212.56,
+          length: 9.536,
           marker: 0,
         },
       ],
@@ -201,6 +201,7 @@ const handleEnd = () => {
 }
 
 const UpdateTime = (timer) => {
+  console.log(timer);
   let timerPercent =
     (timer / state.tuts[state.subject].videos[state.video].length) * 100
   state.tuts[state.subject].videos[state.video].marker = timerPercent
@@ -320,18 +321,18 @@ h1 {
   position: relative;
   height: 100%;
   background-color: #a7a7a7;
-  padding: 25px 10px 100px;
+  padding: 10px;
   z-index: 2;
 }
 .table-of-contents ul {
   list-style: none;
   padding: 0;
-  margin: 0;
   height: 100%;
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-evenly;
-  align-items: center;
+  align-items: flex-start;
+  margin: 0 25%;
 }
 .table-of-contents ul li a {
   font-weight: bold;
