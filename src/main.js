219032e13105d81878e583482d917083a847b8e5
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-10-03 15:18:26 -0400

    added VueGTM and code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtm from 'vue-gtm';
 import router from './router'
 import Particles from "particles.vue3";
 
@@ -8,6 +9,16 @@ import './assets/main.css'
 import { createPinia } from 'pinia'
 
 const app = createApp(App).use(createPinia()).use(Particles)
+
+use(VueGtm, {
+    id: process.env.GTM-5QNGL5T,
+    defer: false,
+    enabled: true,
+    debug: false,
+    loadScript: true,
+    vueRouter: router,
+    trackOnNextTick: false,
+  });
 
 app.use(router)
 
